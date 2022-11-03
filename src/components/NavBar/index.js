5eba7933209e73fb72039205d7781c9c531bf5c6
lmbd92 <lmbd92@hotmail.com>
2022-11-02 20:50:13 -0500

    Implementing navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,2 +1,17 @@
-import NavBar from "./NavBar";
-export default NavBar;
+import React from "react";
+import { Nav, NavLink, Bars, NavMenu } from "./NavbarElements";
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/about">Sobre Nosotros</NavLink>
+          <NavLink to="/home">Home</NavLink>
+        </NavMenu>
+      </Nav>
+    </>
+  );
+};
+export default Navbar;
---
+++ src/containers/Pages/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="style">
+      <h1>GeeksforGeeks is a Computer Science portal for geeks.</h1>
+    </div>
+  );
+};
+
+export default About;
