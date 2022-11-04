b6bb4a61c016b1cad91846a677f754eab63ad88d
Steven Zuluaga <steven000069@hotmail.com>
2022-11-03 22:15:17 -0500

    Adding logo to navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Nav, NavLink, Bars, NavMenu } from "./NavbarElements";
+import logo from "../../Assets/images/Logo.png"
+import "./NavBar.css"
 
 const Navbar = () => {
   return (
@@ -7,7 +9,10 @@ const Navbar = () => {
       <Nav>
         <Bars />
         <NavMenu>
-          <NavLink to="/about">Sobre Nosotros</NavLink>
+          <NavLink to="/home">
+            <img className="logo" src={logo}/>
+          </NavLink>
+          <NavLink to="/about">About Us</NavLink>
           <NavLink to="/home">Home</NavLink>
         </NavMenu>
       </Nav>
