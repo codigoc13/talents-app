aa8ed9d1acb2c3c0082388834056eb863b73fa09
Steven Zuluaga <steven000069@hotmail.com>
2022-11-03 22:15:47 -0500

    Updating styles to navbar

--- src/components/NavBar/NavbarElements.js
+++ src/components/NavBar/NavbarElements.js
@@ -3,12 +3,13 @@ import { NavLink as Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-  background: #ffcb05;
+  background: #fff;
   height: 85px;
   display: flex;
   justify-content: flex-end;
   padding: 0.2rem calc((100vw - 1000px) / 2);
   z-index: 12;
+  
 `;
 
 export const NavLink = styled(Link)`
@@ -18,6 +19,8 @@ export const NavLink = styled(Link)`
   text-decoration: none;
   padding: 0 1rem;
   height: 100%;
+  font-family:coogose-thin;
+  font-weight:bolder;
   cursor: pointer;
   &.active {
     color: #000000;
