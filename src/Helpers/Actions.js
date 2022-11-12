8958bc5618e93c64212f25241e072c8ac6209755
Jhonier Mosquera <jhoniermosquera61@gmail.com>
2022-11-11 19:06:55 -0500

    Desarrollar nuevo diseño de popup

--- src/Helpers/Actions.js
+++ src/Helpers/Actions.js
@@ -12,52 +12,75 @@ export const getAllStudents = () => {
           {
 
                id: 2,
-               name: 'Stiven Arley Pareja Jimenez',
-               avatarUrl: "https://res.cloudinary.com/dpfrys3uj/image/upload/v1667527315/avatars/IMG-20221031-WA0018_lsbkwt.jpg",
+               name: 'Jhonier Andres Mosquera Benitez ',
+               nickName: 'Jhonier Mosquera',
+               avatarUrl: "https://res.cloudinary.com/dk9ktxdpj/image/upload/v1668207814/Jhonier-Mosquera-_2_-sin-fondo_ck3rzu.png",
+               symbolUrl: "https://res.cloudinary.com/dk9ktxdpj/image/upload/v1668094498/Capa_1_jzulai.png",
                program: "FrontEnd",
                CV:"https://drive.google.com/file/d/1s7fR7BkEOjSvDO4Hprj1zN9MI7PWaOHD/view?usp=share_link"
           },
           {
 
                id: 3,
-               name: 'Yinier Smith Mosquera Córdoba ',
+               name: 'Stiven Arley Pareja Jimenez',
+               nickName: 'Stiven Pareja',
                avatarUrl: "https://res.cloudinary.com/dpfrys3uj/image/upload/v1667527315/avatars/IMG-20221031-WA0018_lsbkwt.jpg",
+               symbolUrl: "https://res.cloudinary.com/dk9ktxdpj/image/upload/v1668094498/Capa_1_jzulai.png",
                program: "FrontEnd",
                CV:"https://drive.google.com/file/d/1s7fR7BkEOjSvDO4Hprj1zN9MI7PWaOHD/view?usp=share_link"
           },
           {
 
                id: 4,
-               name: 'Kevin Alexis Jaramillo Potosí',
+               name: 'Yinier Smith Mosquera Córdoba ',
+               nickName:'Yinier Mosquera',
                avatarUrl: "https://res.cloudinary.com/dpfrys3uj/image/upload/v1667527315/avatars/IMG-20221031-WA0018_lsbkwt.jpg",
+               symbolUrl: "https://res.cloudinary.com/dk9ktxdpj/image/upload/v1668094498/Capa_1_jzulai.png",
+               program: "FrontEnd",
+               CV:"https://drive.google.com/file/d/1s7fR7BkEOjSvDO4Hprj1zN9MI7PWaOHD/view?usp=share_link"
+          },
+          {
+
+               id: 5,
+               name: 'Kevin Alexis Jaramillo Potosí',
+               nickName: 'Kevin Jaramillo',
+               avatarUrl: "https://res.cloudinary.com/dpfrys3uj/image/upload/v1667527315/avatars/IMG-20221031-WA0018_lsbkwt.jpg",
+               symbolUrl: "https://res.cloudinary.com/dk9ktxdpj/image/upload/v1668094498/Capa_1_jzulai.png",
                program: "FrontEnd",
                CV:"https://drive.google.com/file/d/1s7fR7BkEOjSvDO4Hprj1zN9MI7PWaOHD/view?usp=share_link"
           },
           
           {
 
-               id: 5,
-               name: 'Wiston Alberto Mosquera Mosquera',
-               avatarUrl: "https://res.cloudinary.com/dpfrys3uj/image/upload/v1667527315/avatars/IMG-20221031-WA0018_lsbkwt.jpg",
-               program: "FrontEnd",
-               CV:"https://drive.google.com/file/d/1s7fR7BkEOjSvDO4Hprj1zN9MI7PWaOHD/view?usp=share_link"
-          },
-          {
-
                id: 6,
-               name: 'Estivinson Murillo Ayala',
-               avatarUrl: "https://res.cloudinary.com/dpfrys3uj/image/upload/v1667531305/avatars/Estivinson_Murillo_A._yzr4ax.jpg",
+               name: 'Wiston Alberto Mosquera Mosquera',
+               nickName: 'Wiston Mosquera',
+               avatarUrl: "https://res.cloudinary.com/dpfrys3uj/image/upload/v1667527315/avatars/IMG-20221031-WA0018_lsbkwt.jpg",
+               symbolUrl: "https://res.cloudinary.com/dk9ktxdpj/image/upload/v1668094498/Capa_1_jzulai.png",
                program: "FrontEnd",
                CV:"https://drive.google.com/file/d/1s7fR7BkEOjSvDO4Hprj1zN9MI7PWaOHD/view?usp=share_link"
           },
           {
 
                id: 7,
-               name: 'Jhon Edwin Asprilla Guisao ',
-               avatarUrl: "https://res.cloudinary.com/dpfrys3uj/image/upload/v1667527315/avatars/IMG-20221031-WA0018_lsbkwt.jpg",
+               name: 'Estivinson Murillo Ayala',
+               nickName: 'Estivinson Murillo',
+               avatarUrl: "https://res.cloudinary.com/dpfrys3uj/image/upload/v1667531305/avatars/Estivinson_Murillo_A._yzr4ax.jpg",
+               symbolUrl: "https://res.cloudinary.com/dk9ktxdpj/image/upload/v1668094498/Capa_1_jzulai.png",
                program: "FrontEnd",
                CV:"https://drive.google.com/file/d/1s7fR7BkEOjSvDO4Hprj1zN9MI7PWaOHD/view?usp=share_link"
-          }
+          },
+          {
+
+               id: 8,
+               name: 'Jhon Edwin Asprilla Guisao ',
+               nickName: 'Jhon Edwin Asprilla',
+               avatarUrl: "https://res.cloudinary.com/dk9ktxdpj/image/upload/v1668208286/WhatsApp-Image-2022-11-09-at-12.12_yeg94s.png",
+               symbolUrl: "https://res.cloudinary.com/dk9ktxdpj/image/upload/v1668094498/Capa_1_jzulai.png",
+               program: "FrontEnd",
+               CV:"https://drive.google.com/file/d/1s7fR7BkEOjSvDO4Hprj1zN9MI7PWaOHD/view?usp=share_link"
+          },
+          
      ]
      return students
 }
